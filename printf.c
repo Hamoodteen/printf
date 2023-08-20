01ea80bc39aaf8552c681dd0b16dded56ceb3b50
Hamoodteen <ahmed104567@outlook.com>
2023-08-21 01:48:03 +0300

    0

--- printf.c
+++ printf.c
@@ -33,6 +33,9 @@ int _printf(const char *format, ...)
 			case 'd':
 				print_number(va_arg(list, int));
 				break;
+			case 'i':
+				print_number(va_arg(list, int));
+				break;
 			}
 		}
 		else
@@ -43,11 +46,21 @@ int _printf(const char *format, ...)
 }
 int main(void)
 {
+	int len;
+    int len2;
+	len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
 	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-
-	_printf("String:[%s]\n");
-	printf("String:[%s]\n");
-
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
 	return (0);
 }
