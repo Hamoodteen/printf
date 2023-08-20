cac24e8a3537851a348289634cdcdf8ef6351736
Hamoodteen <ahmed104567@outlook.com>
2023-08-20 15:26:54 +0300

    0

--- main.h
+++ main.h
@@ -1,4 +1,5 @@
 #ifndef _HEADER_
 #define _HEADER_
-
+int _putchar(char c);
+int _printf(const char *format, ...);
 #endif
---
+++ printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _printf - f
+ * @format: char
+ * @...: args
+ * Return: int
+*/
+int _printf(const char *format, ...)
+{
+
+}
