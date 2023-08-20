be9d9cd0c52df896d98a7dbaba8f1975edbb27f6
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-20 21:58:28 +0300

    done

--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -10,12 +9,6 @@
 
 int _putchar(char c);
 int _puts(char *c);
-=======
-#include "_putc.c"
-#include <string.h>
-#include <stdarg.h>
-
-int _putc(char c);
-void _puts(const char *);
->>>>>>> fa95ba13eb6bace539a6a1c1c123a61c7c90bc36
 int _printf(const char *format, ...);
+
+#endif
