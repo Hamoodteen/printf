a7126ca60726a444550485a7fee06d52b03f9ae0
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-20 18:38:25 +0300

    done

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+#include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-#include <unistd.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
