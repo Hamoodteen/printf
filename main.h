9b05728713dc34d0a86a01835df7c89953069243
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-20 22:41:46 +0300

    done

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "_putchar.c"
-#include "_puts.c"
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "_putchar.c"
+#include "_puts.c"
 
 int _putchar(char c);
 int _puts(char *c);
