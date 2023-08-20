f0c82e69cb316616f9fd1d087125eb403fe585e0
Hamoodteen <ahmed104567@outlook.com>
2023-08-20 21:57:33 +0300

    0

--- printf.c
+++ printf.c
@@ -8,65 +8,18 @@
 */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	int i;
-	char *strng;
-	va_list list;
-
-	va_start(list, format);
-
-	for (i = 0; format[i] != '\0'; i++)
-=======
 	va_list args;
 	int i;
 
 	va_start(args, format);
 	i = 0;
 	while (format[i])
->>>>>>> fa95ba13eb6bace539a6a1c1c123a61c7c90bc36
 	{
 		if (format[i] == '%')
 		{
 			i++;
 			switch (format[i])
 			{
-<<<<<<< HEAD
-				case 'c':
-					_putchar(va_arg(list, int));
-					break;
-				case 's':
-					strng = va_arg(list, char *);
-					if (!strng)
-						strng = "(nil)";
-					_puts(strng);
-					break;
-			}
-		}
-		else
-			_putchar(format[i]);
-	}
-	va_end(list);
-
-	return (strlen(format) + 1);
-}
-/**
- * main - call printf
- *
- * Return: int
-*/
-int main(void)
-{
-	char l = 'b';
-	char *str;
-
-	str = "bbb";
-
-	_printf("s%casa\n", l);
-	_printf("s%sasa", str);
-
-	return (0);
-}
-=======
 			case 'c':
 				_putc(va_arg(args, int));
 				break;
@@ -92,4 +45,3 @@ int main(void)
 	va_end(args);
 	return (strlen(format));
 }
->>>>>>> fa95ba13eb6bace539a6a1c1c123a61c7c90bc36
