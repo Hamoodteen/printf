527710dc566856271a9ad1e04cc1a3a8eb214cd2
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-20 23:25:34 +0300

    done

--- printf.c
+++ printf.c
@@ -39,27 +39,3 @@ int _printf(const char *format, ...)
 
 	return (len);
 }
-int main(void)
-{
-    int len;
-    int len2;
-
-
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    len = _printf("Let's try to printf a simple sentence.fff\n");
-
-
-    printf("Character:[%c]\n", 'H');
-    _printf("Character:[%c]fff\n", 'H');
-
-    printf("String:[%s]\n", "I am a string !");
-    _printf("String:[%s]fff\n", "I am a string !");
-
-    len2 = printf("Percent:[%%]\n");
-    len = _printf("Percent:[%%]fff\n");
-
-    printf("Len:[%d]\n", len2);
-    _printf("Len:[%d]fff\n", len);
-    return (0);
-}
-
