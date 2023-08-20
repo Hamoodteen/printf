f9ca8d4d56f692409e7621258b8e06ae0edd5694
Hamoodteen <ahmed104567@outlook.com>
2023-08-21 01:43:29 +0300

    0

--- print_number.c
+++ print_number.c
@@ -1,1 +1,35 @@
 
+#include "main.h"
+/**
+ * print_number - f
+ * @n: int
+ * Return: int
+*/
+int print_number(int n)
+{
+	int divisor;
+	int tmp;
+	int cnt = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		cnt++;
+	}
+	divisor = 1;
+	tmp = n;
+	while (tmp / 10 > 0)
+	{
+		divisor *= 10;
+		tmp /= 10;
+		cnt++;
+	}
+	while (divisor > 0)
+	{
+		_putchar('0' + n / divisor);
+		n %= divisor;
+		divisor /= 10;
+	}
+	return (cnt);
+}
