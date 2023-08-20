8679504d7b4e0fd489fd9e3f864d137bddbb4560
Hamoodteen <ahmed104567@outlook.com>
2023-08-20 19:45:17 +0300

    0

--- _putchar.c
+++ _putchar.c
@@ -1,15 +1,21 @@
 #include <unistd.h>
 #include <string.h>
-
 /**
- * _puts - prints a string
- * @str: pointer to string to print
- * Return: none
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * _puts - prints a string
+ * @str: pointer to string to print
+ * Return: none
+ */
 void _puts(const char *str)
 {
 	write(1, str, strlen(str));
