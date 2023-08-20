22471d8ec4680d50d78225cc5dc163db593e32d8
Hamoodteen <ahmed104567@outlook.com>
2023-08-21 00:55:40 +0300

    0

--- printf.c
+++ printf.c
@@ -8,59 +8,38 @@
 */
 int _printf(const char *format, ...)
 {
-	int i;
-	int len;
-	va_list list;
+  int i;
+  int len;
+  va_list list;
 
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					len += _putchar(va_arg(list, int));
-					break;
-				case 's':
-					len += _puts(va_arg(list, char *));
-					break;
-				case '%':
-					len += _putchar('%');
-					break;
-				case 'd':
-					len += print_number(va_arg(list, int));
-					break;
-			}
-		}
-		else
-			len += _putchar(format[i]);
-	}
-	va_end(list);
+  if (format == NULL)
+    return (-1);
+  va_start(list, format);
+  for (i = 0; format[i] != '\0'; i++)
+  {
+    if (format[i] == '%')
+    {
+      i++;
+      switch (format[i])
+      {
+        case 'c':
+          len += _putchar(va_arg(list, int));
+          break;
+        case 's':
+          len += _puts(va_arg(list, char *));
+          break;
+        case '%':
+          len += _putchar('%');
+          break;
+        case 'd':
+          len += print_number(va_arg(list, int));
+          break;
+      }
+    }
+    else
+      len += _putchar(format[i]);
+  }
+  va_end(list);
 
-	return (len);
-}
-int main(void)
-{
-int len;
-    int len2;
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    return (0);
+  return (len);
 }
