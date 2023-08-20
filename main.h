6cc8ecdf7606ce9d6895d5998e7fde31ef0f028f
Hamoodteen <ahmed104567@outlook.com>
2023-08-20 21:16:23 +0300

    0

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define _HEADER_
 
 #include <stdio.h>
+#include "_putchar.c"
 #include <string.h>
 #include <stdarg.h>
 
@@ -9,24 +10,4 @@ int _putchar(char c);
 void _puts(const char *);
 int _printf(const char *format, ...);
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * _puts - prints a string
- * @str: pointer to string to print
- * Return: void
- */
-void _puts(const char *str)
-{
-	write(1, str, strlen(str));
-}
 #endif
