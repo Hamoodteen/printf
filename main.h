1485a8a5139d20b1fbb1770f8600e37f8fb076da
Hamoodteen <ahmed104567@outlook.com>
2023-08-20 15:30:20 +0300

    0

--- main.h
+++ main.h
@@ -1,5 +1,7 @@
 #ifndef _HEADER_
 #define _HEADER_
+#include <unistd.h>
+#include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
 #endif
--- printf.c
+++ printf.c
@@ -7,5 +7,10 @@
 */
 int _printf(const char *format, ...)
 {
+	va_list args;
+	unsigned int i = 0;
 
+	va_start(args, format);
+	va_end(args);
+	return (args);
 }
