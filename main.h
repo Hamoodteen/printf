eb1503118c2a8c303363056e87110cbc8d625e9e
Hamoodteen <ahmed104567@outlook.com>
2023-08-20 21:14:23 +0300

    0

--- main.h
+++ main.h
@@ -10,4 +10,24 @@ int _putchar(char c);
 void _puts(const char *);
 int _printf(const char *format, ...);
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _puts - prints a string
+ * @str: pointer to string to print
+ * Return: void
+ */
+void _puts(const char *str)
+{
+	write(1, str, strlen(str));
+}
 #endif
