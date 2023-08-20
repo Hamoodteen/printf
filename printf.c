d15546fa59409f789da429da7f11f9cdcadc54e1
Hamoodteen <ahmed104567@outlook.com>
2023-08-21 00:59:51 +0300

    0

--- printf.c
+++ printf.c
@@ -1,45 +1,43 @@
 #include "main.h"
-
 /**
  * _printf - f
  * @format: char
  * @...: args
  * Return: int
-*/
+ */
 int _printf(const char *format, ...)
 {
-  int i;
-  int len;
-  va_list list;
+	int i;
+	int len;
+	va_list list;
 
-  if (format == NULL)
-    return (-1);
-  va_start(list, format);
-  for (i = 0; format[i] != '\0'; i++)
-  {
-    if (format[i] == '%')
-    {
-      i++;
-      switch (format[i])
-      {
-        case 'c':
-          len += _putchar(va_arg(list, int));
-          break;
-        case 's':
-          len += _puts(va_arg(list, char *));
-          break;
-        case '%':
-          len += _putchar('%');
-          break;
-        case 'd':
-          len += print_number(va_arg(list, int));
-          break;
-      }
-    }
-    else
-      len += _putchar(format[i]);
-  }
-  va_end(list);
-
-  return (len);
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+			case 'c':
+				len += _putchar(va_arg(list, int));
+				break;
+			case 's':
+				len += _puts(va_arg(list, char *));
+				break;
+			case '%':
+				len += _putchar('%');
+				break;
+			case 'd':
+				len += print_number(va_arg(list, int));
+				break;
+			}
+		}
+		else
+			len += _putchar(format[i]);
+	}
+	va_end(list);
+	return (len);
 }
