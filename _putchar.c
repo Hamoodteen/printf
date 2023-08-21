925b26ad832539f1611f4bdb8f93f69ba7bf2346
Hamoodteen <ahmed104567@outlook.com>
2023-08-21 18:18:04 +0300

    0

--- _putchar.c
+++ _putchar.c
@@ -6,5 +6,47 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buffer[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buffer, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buffer[i] = c;
+		i++;
+	}
+	return (1);
+}
+/**
+ * _strlen - do something
+ * @s: int or char
+ * Return: some thing
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * _puts - prints a string
+ * @c: pointer to string to print
+ * Return: string
+ */
+int _puts(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+		_putchar(c[i]);
+	return (i);
 }
