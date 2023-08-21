db43d6c1ac70bb1df3c73eed7a8f274eb3d3ab92
Hamoodteen <ahmed104567@outlook.com>
2023-08-21 18:20:45 +0300

    0

--- _putchar.c
+++ _putchar.c
@@ -6,20 +6,7 @@
  */
 int _putchar(char c)
 {
-	static char buffer[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buffer, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buffer[i] = c;
-		i++;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
 /**
  * _strlen - do something
@@ -44,9 +31,5 @@ int _strlen(const char *s)
  */
 int _puts(char *c)
 {
-	int i;
-
-	for (i = 0; c[i] != '\0'; i++)
-		_putchar(c[i]);
-	return (i);
+	return (write(1, &c[0], _strlen(c)));
 }
