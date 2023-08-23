2c2ab65fac6e0a2ab5d6fb02e56171191497d82c
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-23 07:57:07 +0300

    done

--- main.h
+++ main.h
@@ -7,9 +7,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
-#include "myFunctions1.c"
-#include "myFunctions2.c"
-#include "myFunctions3.c"
 
 int _putchar(char c);
 int _puts(char *c);
